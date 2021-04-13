378d40bed797a82fedd415b2744b30b0dd0f1afe
nicodeclercq <nicolas.declercq@iadvize.com>
2021-04-13 14:34:14 +0200

    add welcome message

--- src/store/v1.tsx
+++ src/store/v1.tsx
@@ -1,4 +1,6 @@
 import * as IO from 'io-ts';
+import { prompt } from '../helpers/io';
+import { Button } from '../components/Button';
 
 export type Target = 'Animal' | 'Object' | 'Person' | 'Plant';
 export type Element = 'Feu' | 'Air' | 'Eau' | 'Terre' | 'Âme' | 'Corps';
@@ -51,8 +53,33 @@ export const stateDecoder = IO.strict({
   uses: IO.record(IO.string, IO.number),
 });
 
+const Welcome = ({callback}: {callback: () => void}) => (
+  <div className="flex flex-col space-y-4 p-2">
+    <p>
+      Avant de commencer sache que tout ce que tu inscris ici est <strong>sauvegardé en cache sur ton navigateur.
+      </strong> Ainsi si tu changes de navigateur, d'ordinateur ou qu'il se passe quoi que ce soit d'imprévue
+      (les bugs ici n'existant évidemment pas) tu pourras dire avec nous cette magnifique phrases dont la véracité n'a d'égale
+      que sa douce simplicité « C’est balot! ».
+    </p>
+    <div>
+      <div className="flex flex-row space-x-4 p-2">
+        <input id="🙈" type="checkbox"/><label htmlFor="🙈">J'accepte l'inacceptable *</label>
+      </div>
+    </div>
+    <Button onClick={callback} type="primary">En avant!</Button>
+    <small>* Cette case à cocher ne sert à rien mais c'est devenue à la mode du coup j'en met une aussi, ça fait tout de suite plus pro vous ne trouvez pas?</small>
+  </div>
+);
+
 function update(_currentState: unknown): Promise<State> {
-  return Promise.resolve(defaultState)
+  return Promise.resolve()
+    .then(
+      () => prompt<void>(
+        (callback: () => void) => <Welcome callback={callback} />,
+        <>Bienvenue sur ta fiche de personnage</>
+      )
+    )
+    .then(() => defaultState)
 }
 
 export function retrieve(currentState: unknown): Promise<State> {
