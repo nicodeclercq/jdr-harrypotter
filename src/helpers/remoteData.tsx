cc3d06f5cb6c3b8d4ef8a363626b20c8e411e2d6
nicodeclercq <nicolas.declercq@iadvize.com>
2021-04-14 19:17:03 +0200

    fix remoteData

--- src/helpers/remoteData.tsx
+++ src/helpers/remoteData.tsx
@@ -3,14 +3,11 @@ import * as RemoteData from "@devexperts/remote-data-ts";
 import { pipe } from "fp-ts/lib/function";
 
 import { Loader } from "../components/Loader";
-import { Layout } from "../components/Layout";
 
 const Load = () => (
-  <Layout>
-    <div className="flex justify-center align-center h-full w-full">
-      <Loader />
-    </div>
-  </Layout>
+  <div className="flex justify-center align-center h-full w-full">
+    <Loader />
+  </div>
 );
 
 export const fromRemoteData = <T, U>(remoteData: RemoteData.RemoteData<T, U>, callback: (data: U) => JSX.Element ): JSX.Element => pipe(
