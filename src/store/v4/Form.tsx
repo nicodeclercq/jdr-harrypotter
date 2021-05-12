98b84540e40b6db4063180ddd2c64643918b3d2e
nicodeclercq <nicolas.declercq@iadvize.com>
2021-05-12 10:34:05 +0200

    fix skills

--- src/store/v4/Form.tsx
+++ src/store/v4/Form.tsx
@@ -13,9 +13,8 @@ const defaultSkills = entries(skills);
 const repartitionPoints = 300;
 
 export function Form({state: { traits }, callback}: {state: State,callback: (result: {skills: Skills}) => void}) {
-  const initialPoints = defaultSkills.reduce((acc, [,{ min, baseTrait }]) => acc + (traits[baseTrait] > min ? traits[baseTrait] : min), 0);
   const [remainingPoints, setRemainingPoints] = useState(repartitionPoints);
-  const { handleSubmit, control, errors, setValue } = useForm<Record<string, number>>({
+  const { handleSubmit, control, errors, setValue, getValues } = useForm<Record<string, number>>({
     defaultValues: defaultSkills.reduce((acc, [name, {min, baseTrait}]) => ({
         ...acc,
         [name]: traits[baseTrait] > min
@@ -23,8 +22,32 @@ export function Form({state: { traits }, callback}: {state: State,callback: (res
           : min
       }),
       {} as Record<string, number>
-    )
+    ),
+    resolver: (values: Record<string, number>) => new Promise((resolve, reject) => {
+      if(remainingPoints !== 0){
+        const firstEntry = entries(values)[0][0];
+        resolve({
+          values: {},
+          // @ts-ignore
+          errors: {
+            [firstEntry]: 'Ça doit faire zéro',
+          }
+        });
+      } else {
+        resolve({values, errors: {}});
+      }
+    }),
   });
+
+  const onSkillChange = (skill: string, callback: (value: number) => void) => (value: string) => {
+    const currentValue = getValues()[skill];
+    const v = parseInt(value, 10);
+    setRemainingPoints(remainingPoints - v + currentValue);
+    callback(v);
+  };
+  const setSkillValue = (skill: string, value: number) => {
+    onSkillChange(skill, (v) => setValue(skill, v))(`${value}`);
+  }
 
   const onSubmit = (results: Record<string, number>) => {
     const skills = entries(results)
@@ -40,31 +63,34 @@ export function Form({state: { traits }, callback}: {state: State,callback: (res
     callback({skills});
   }
 
+  const getMin = (skill: string) => traits[skills[skill].baseTrait] > skills[skill].min ? traits[skills[skill].baseTrait] : skills[skill].min;
+  const getMax = (skill: string) => traits[skills[skill].baseTrait] * 5;
+
   return (
     <form  onSubmit={handleSubmit(onSubmit)}>
       <div className="w-full">
           <div className="overflow-y-auto" style={{height: '70vh'}}>
             {
-              defaultSkills.map(([skill, { baseTrait, min }]) => (
+              defaultSkills.map(([skill]) => (
                 <div className="flex flex-row p-2 border-gray-100 space-x-3 border-b-1">
                   <Label htmlFor={`input-${skill}`}>{skill}</Label>
                   <Controller
                     name={skill}
                     control={control}
-                    rules={{ required: true, min: traits[baseTrait] > min ? traits[baseTrait] : min, max: traits[baseTrait] * 5 }}
+                    rules={{ required: true, min: getMin(skill), max: getMax(skill) }}
                     render={({value, onChange}) => (<>
-                      <Button type="secondary" onClick={() => setValue(skill, traits[baseTrait])}>Min ({traits[baseTrait]}%)</Button>
+                      <Button type="secondary" onClick={() => setSkillValue(skill, getMin(skill))}>Min ({getMin(skill)}%)</Button>
                       <Input
                         id={`input-${skill}`}
                         errors={errors[skill]}
                         value={value}
                         type="number"
                         theme="neutral"
-                        max={traits[baseTrait] * 5}
-                        min={traits[baseTrait] > min ? traits[baseTrait] : min}
-                        onChange={onChange}
+                        max={getMax(skill)}
+                        min={getMin(skill)}
+                        onChange={onSkillChange(skill, onChange)}
                       />
-                      <Button type="secondary" onClick={() => setValue(skill, traits[baseTrait] * 5)}>Max ({traits[baseTrait] * 5}%)</Button>
+                      <Button type="secondary" onClick={() => setSkillValue(skill, getMax(skill))}>Max ({getMax(skill)}%)</Button>
                     </>)}
                   />
                 </div>
@@ -73,7 +99,13 @@ export function Form({state: { traits }, callback}: {state: State,callback: (res
           </div>
           <div className="space-x-2">
             <Button onClick="submit" type="primary">Valider</Button>
-            <span>{remainingPoints}/{repartitionPoints} points à répartir</span>
+            <span className={
+              remainingPoints < 0
+                ? 'text-red-500'
+                : remainingPoints === 0
+                  ? 'text-green-500'
+                  : ''
+            }>{remainingPoints}/{repartitionPoints} points à répartir</span>
           </div>
       </div>
     </form>
