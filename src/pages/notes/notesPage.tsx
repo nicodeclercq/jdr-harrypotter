74e938407560409776a4c6fa9b7d00aebc327cf4
nicodeclercq <nicolas.declercq@iadvize.com>
2021-07-01 18:24:38 +0200

    add filter on notes

--- src/pages/notes/notesPage.tsx
+++ src/pages/notes/notesPage.tsx
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
 import { Layout } from '../../components/Layout';
 import { fromRemoteData } from '../../helpers/remoteData';
+import { State } from '../../store/State';
 import { Note } from './Note';
 import { useNote } from './useNote';
 
+
+function Notes({notes}: { notes: State['notes']}) {
+  const { addNote } = useNote();
+  const [displayedNotes, setDisplayedNotes] = useState(notes);
+
+  const search = (query: string) => {
+    setDisplayedNotes(notes.filter(note => note.title.includes(query) || note.description.includes(query)));
+  }
+
+  return (<div className="space-y-4">
+    <div className="flex items-center justify-center w-full">
+      <Input placeholder="🔎 Rechercher" type="search" theme="neutral" onChange={search} width="50%" />
+    </div>
+    <div className="w-full max-h-full py-2 overflow-y-auto bg-white rounded shadow-m">
+      <div className="p-2 grid grid-cols-2 gap-2">
+        {
+          displayedNotes.map((note) => (
+            <Note key={note.id} id={note.id} description={note.description} title={note.title} />
+          ))
+        }
+      </div>
+      <div className="flex justify-center w-full pt-4">
+        <Button type="primary" onClick={addNote}>Ajouter une note</Button>
+      </div>
+    </div>
+  </div>)
+}
+
 export function NotesPage() {
-  const { getNotes, addNote } = useNote();
+  const { getNotes } = useNote();
 
   return fromRemoteData(
     getNotes(),
     (notes) => (
     <Layout>
       <div className="w-full h-full">
-        <div className="w-full max-h-full py-2 overflow-y-auto bg-white rounded shadow-m">
-          <div className="p-2 grid grid-cols-2 gap-2">
-            {
-              notes.map((note) => (
-                <Note key={note.id} id={note.id} description={note.description} title={note.title} />
-              ))
-            }
-          </div>
-          <div className="flex justify-center w-full pt-4">
-            <Button type="primary" onClick={addNote}>Ajouter une note</Button>
-          </div>
-        </div>
+        <Notes notes={notes} />
       </div>
     </Layout>
   ))
