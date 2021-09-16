9884538c39107a129883806c2f6ba4c7cb9ac0e3
nicodeclercq <nicolas.declercq@iadvize.com>
2021-09-16 20:08:18 +0200

    fix avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -7,9 +7,18 @@ type Props = {
   onClick?: () => void;
 }
 
+const randomColor = (str: string) => {
+  const value = str
+    .split('')
+    .map(char => char.charCodeAt(0))
+    .reduce((acc, cur) => acc + cur, 0);
+  const hue = value % 360;
+  return `hsl(${hue}, 25%, 50%)`;
+}
+
 export function Avatar({text, url, onClick}: Props) {
   return (
-    <div title={text} onClick={onClick} className="flex items-center justify-center flex-none w-12 h-12 border-2 border-white rounded-full shadow" style={{ backgroundImage: `url("${url}")`, backgroundPosition: 'center', backgroundSize: 'cover'}}>
+    <div title={text} onClick={onClick} className="flex items-center justify-center flex-none w-12 h-12 border-2 border-white rounded-full shadow" style={{ flex: 'none', backgroundColor: randomColor(text), backgroundImage: `url("${url}")`, backgroundPosition: 'center', backgroundSize: 'cover'}}>
       {url ? '' : <Icon name="SORCERER" />}
     </div>
   );
