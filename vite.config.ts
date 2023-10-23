16f92d584c59b8150d4cd9cd8b403273527145e4
nicodeclercq <nicolas.declercq@doctolib.com>
2023-10-23 20:55:59 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
