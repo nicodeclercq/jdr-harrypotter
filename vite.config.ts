0ac2826925a54811761e90d986b03a5500296221
nicodeclercq <nicolas.declercq@doctolib.com>
2023-10-26 10:46:06 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: "build",
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
       output: {
