134ab595df041f1df2e95c14b42cbc100dd42db7
nicodeclercq <nicolas.declercq@doctolib.com>
2024-01-03 20:25:46 +0100

    add update time

--- src/store/externalStores/providers/firebase.ts
+++ src/store/externalStores/providers/firebase.ts
@@ -3,8 +3,14 @@ import { constVoid, constant } from "fp-ts/function";
 import * as Option from "fp-ts/Option";
 import { initializeApp } from "firebase/app";
 import { set, ref, getDatabase, onValue } from "firebase/database";
-import { getFirestore, collection, getDocs, addDoc, deleteDoc, setDoc } from "firebase/firestore/lite";
-
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  setDoc,
+} from "firebase/firestore/lite";
 
 import { secrets } from "../../../secrets";
 import { encryptStore } from "../encryptStore";
@@ -25,55 +31,65 @@ export const io = () => {
   const db = getDatabase();
   const messagesRef = ref(db, "messages");
 
-  const emit = (type: string, message: string)  => {
-    set(messagesRef, {type, payload: message});
+  const emit = (type: string, message: string) => {
+    set(messagesRef, { type, payload: message });
   };
 
   const on = (type: string, callback: (data: string) => void) => {
     onValue(messagesRef, (snapshot) => {
       const data = snapshot.val();
-      if(IO.type({type: IO.literal(type), payload: IO.string}).is(data)) {
+      if (IO.type({ type: IO.literal(type), payload: IO.string }).is(data)) {
         callback(data.payload);
       }
     });
   };
 
-  return {emit, on};
+  return { emit, on };
 };
 
-const getCollection = () => Promise.resolve(collection(db, secrets.firebaseCollectionId));
-const getDoc = (name: string) => getCollection()
-  .then(getDocs)
-  .then(({docs}) => docs)
-  .then(docs => docs.filter(d => d.data().name === name))
-  .then(docs => docs.length > 0
-    ? Option.some(docs[0])
-    : Option.none,
-  );
+const getCollection = () =>
+  Promise.resolve(collection(db, secrets.firebaseCollectionId));
+const getDoc = (name: string) =>
+  getCollection()
+    .then(getDocs)
+    .then(({ docs }) => docs)
+    .then((docs) => docs.filter((d) => d.data().name === name))
+    .then((docs) => (docs.length > 0 ? Option.some(docs[0]) : Option.none));
 
 export const FirebaseStore: CryptedExternalStore = encryptStore({
-  getEntries: () => getCollection()
-    .then(getDocs)
-    .then(({docs}) => docs)
-    .then(docs => docs.map(d => d.get("name"))),
+  getEntries: () =>
+    getCollection()
+      .then(getDocs)
+      .then(({ docs }) => docs)
+      .then((docs) => docs.map((d) => d.get("name"))),
 
-  create: (name) => getCollection()
-    .then(collection => addDoc(collection, { name, value: ""}))
-    .then(constVoid),
+  create: (name) =>
+    getCollection()
+      .then((collection) =>
+        addDoc(collection, {
+          name,
+          value: "",
+          updatedAt: new Date().toISOString(),
+        })
+      )
+      .then(constVoid),
 
-  delete: (name) => getDoc(name)
-    .then(Option.fold(
-      constVoid,
-      (doc) => deleteDoc(doc.ref),
-    )),
+  delete: (name) =>
+    getDoc(name).then(Option.fold(constVoid, (doc) => deleteDoc(doc.ref))),
 
-  read: (name) => getDoc(name)
-    .then(Option.map(doc => doc.get("value")))
-    .then(Option.getOrElse(constant(""))),
+  read: (name) =>
+    getDoc(name)
+      .then(Option.map((doc) => doc.get("value")))
+      .then(Option.getOrElse(constant(""))),
 
-  update: (name, state: string) => getDoc(name)
-    .then(Option.fold(
-      constVoid,
-      (doc) => setDoc(doc.ref, { name, value: state}),
-    )),
+  update: (name, state: string) =>
+    getDoc(name).then(
+      Option.fold(constVoid, (doc) =>
+        setDoc(doc.ref, {
+          name,
+          value: state,
+          updatedAt: new Date().toISOString(),
+        })
+      )
+    ),
 });
