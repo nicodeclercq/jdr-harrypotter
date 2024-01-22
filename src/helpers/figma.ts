e44e4247ed227365d91d5419fa28cb420347b52f
nicodeclercq <nicolas.declercq@doctolib.com>
2024-01-22 15:10:18 +0100

    fix image loading

--- src/helpers/figma.ts
+++ src/helpers/figma.ts
@@ -4,6 +4,13 @@ import { identity } from "fp-ts/function";
 
 import { secrets } from "../secrets";
 
+const log =
+  (msg: string) =>
+  <A>(a: A) => {
+    console.log(msg, a);
+    return a;
+  };
+
 const componentDecoder = IO.type({
   type: IO.literal("COMPONENT"),
 });
@@ -36,56 +43,70 @@ const fileDecoder = IO.type({
 type File = IO.TypeOf<typeof fileDecoder>;
 
 const imagesDecoder = IO.type({
-  images: IO.record(IO.string, IO.string)
+  images: IO.record(IO.string, IO.string),
 });
 
 const findAllFrames = (file: File) =>
-  file.document.children.reduce((acc, cur) => acc.concat(
-    cur.children
-      .filter(frameDecoder.is)
-      .reduce((acc, cur) => {
-        acc.push(cur);
-        return acc;
-      }, [] as Frame[])
-  ), [] as Frame[]);
+  file.document.children.reduce(
+    (acc, cur) =>
+      acc.concat(
+        cur.children.filter(frameDecoder.is).reduce((acc, cur) => {
+          acc.push(cur);
+          return acc;
+        }, [] as Frame[])
+      ),
+    [] as Frame[]
+  );
 
-const getFigmaNode = (fileKey: string, nodeIds: string[]) => {
+const getFigmaNode = (fileKey: string, nodeId: string) => {
   return fetch(
-    `https://api.figma.com/v1/images/${fileKey}?ids=${nodeIds.join(",")}&format=png`,
+    `https://api.figma.com/v1/images/${fileKey}?ids=${nodeId}&format=png`,
     { headers: { "X-Figma-Token": secrets.figmaApiKey } }
   )
-    .then(response => response.json());
+    .then((response) => response.json())
+    .then(log("getFigmaNode"));
 };
 
 const getFigmaFile = (fileKey: string) => {
-  return fetch(
-    `https://api.figma.com/v1/files/${fileKey}`,
-    { headers: { "X-Figma-Token": secrets.figmaApiKey } }
-  )
-    .then(response => response.json());
+  return fetch(`https://api.figma.com/v1/files/${fileKey}`, {
+    headers: { "X-Figma-Token": secrets.figmaApiKey },
+  })
+    .then((response) => response.json())
+    .then(log("getFigmaFile"));
 };
 
-export const getFrames = (fileKey: string) => getFigmaFile(fileKey)
-  .then(fileDecoder.decode)
-  .then(Either.map(findAllFrames))
-  .then(Either.fold(
-    (e) => {
-      throw e;
-    },
-    identity,
-  ))
-  .then(frames => {
-    const ids = frames.map(({id}) => id);
-    return getFigmaNode(fileKey, ids)
-      .then(imagesDecoder.decode)
-      .then(Either.fold(
-        (e) => {
-          throw e;
-        },
-        identity,
-      ))
-      .then(result => Object.values(result.images))
-      .then(images => images.map((image, index) => ({image, name: frames[index].name})));
-  });
-  
-  
+export const getFrames = (fileKey: string) =>
+  getFigmaFile(fileKey)
+    .then(fileDecoder.decode)
+    .then(Either.map(findAllFrames))
+    .then(
+      Either.fold((e) => {
+        throw e;
+      }, identity)
+    )
+    .then((frames) => {
+      const ids = frames.map(({ id }) => id);
+      return Promise.allSettled(
+        ids.map((id) =>
+          getFigmaNode(fileKey, id)
+            .then(imagesDecoder.decode)
+            .then(
+              Either.fold((e) => {
+                throw e;
+              }, identity)
+            )
+            .then(log("image"))
+            .then((result) => Object.values(result.images))
+        )
+      )
+        .then((result) =>
+          result.reduce(
+            (acc, cur) =>
+              cur.status === "fulfilled" ? [...acc, ...cur.value] : acc,
+            [] as string[]
+          )
+        )
+        .then((images) =>
+          images.map((image, index) => ({ image, name: frames[index].name }))
+        );
+    });
